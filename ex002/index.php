029772219317a6b1a31344d7213b1a178883a33b
JANICLEITON FABRICIO DE OLIVEIRA <cleiton.poping2012@hotmail.com>
2025-07-02 15:50:51 -0300

    php

--- ex002/index.php
+++ ex002/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Exemplo de PHP</h1>
-    <?php
+    <?
         date_default_timezone_set("America/Sao_Paulo"); 
         echo "Hoje é dia " . date("d/M/Y");
         echo "e a hora atual é " . date("G:i:s T");
